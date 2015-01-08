67309d891aece09103229fa585a3179eab9c45ab
Cory Kelly <coryplusplus@gmail.com>
2015-01-08 16:01:54 -0600

    Initial Check In

---
+++ src/game/Constants.java
@@ -0,0 +1,61 @@
+package game;
+
+public class Constants {
+
+	public static String DUCK1 = "etc/images/duck1.PNG";
+	public static String DUCK2 = "etc/images/duck1.PNG";
+
+	public static final String SHOTS_0 = "etc/images/shots0.jpg";
+	public static final String SHOTS_1 = "etc/images/shots1.jpg";
+	public static final String SHOTS_2 = "etc/images/shots2.jpg";
+	public static final String SHOTS_3 = "etc/images/shots3.jpg";
+
+	public static final String ROUND_1 = "etc/images/round1.jpg";
+	public static final String ROUND_2 = "etc/images/round2.jpg";
+	public static final String ROUND_3 = "etc/images/round3.jpg";
+
+	public static String HIT_0 = "etc/images/hit0.jpg";
+	public static String HIT_1 = "etc/images/hit1.jpg";
+	public static String HIT_2 = "etc/images/hit2.jpg";
+	public static String HIT_3 = "etc/images/hit3.jpg";
+	public static String HIT_4 = "etc/images/hit4.jpg";
+	public static String HIT_5 = "etc/images/hit5.jpg";
+	public static String HIT_6 = "etc/images/hit6.jpg";
+	public static String HIT_7 = "etc/images/hit7.jpg";
+	public static String HIT_8 = "etc/images/hit8.jpg";
+	public static String HIT_9 = "etc/images/hit9.jpg";
+	public static String HIT_10 = "etc/images/hit10.jpg";
+
+	public static String SCORE_0 = "etc/images/score0.jpg";
+	public static String SCORE_10 = "etc/images/score10.jpg";
+	public static String SCORE_20 = "etc/images/score20.jpg";
+	public static String SCORE_30 = "etc/images/score30.jpg";
+	public static String SCORE_40 = "etc/images/score40.jpg";
+	public static String SCORE_50 = "etc/images/score50.jpg";
+	public static String SCORE_60 = "etc/images/score60.jpg";
+	public static String SCORE_70 = "etc/images/score70.jpg";
+	public static String SCORE_80 = "etc/images/score80.jpg";
+	public static String SCORE_90 = "etc/images/score90.jpg";
+	public static String SCORE_100 = "etc/images/score100.jpg";
+	public static String SCORE_200 = "etc/images/score200.jpg";
+	public static String SCORE_300 = "etc/images/score300.jpg";
+	public static String SCORE_400 = "etc/images/score400.jpg";
+	public static String SCORE_500 = "etc/images/score500.jpg";
+	public static String SCORE_600 = "etc/images/score600.jpg";
+	public static String SCORE_700 = "etc/images/score700.jpg";
+	public static String SCORE_800 = "etc/images/score800.jpg";
+	public static String SCORE_900 = "etc/images/score900.jpg";
+	public static String SCORE_1000 = "etc/images/score1000.jpg";
+	public static String SCORE_2000 = "etc/images/score2000.jpg";
+	public static String SCORE_3000 = "etc/images/score3000.jpg";
+	public static String SCORE_4000 = "etc/images/score4000.jpg";
+	public static String SCORE_5000 = "etc/images/score5000.jpg";
+	public static String SCORE_6000 = "etc/images/score6000.jpg";
+	public static String SCORE_7000 = "etc/images/score7000.jpg";
+	public static String SCORE_8000 = "etc/images/score8000.jpg";
+	public static String SCORE_9000 = "etc/images/score9000.jpg";
+	public static String SCORE_10000 = "etc/images/score10000.jpg";
+	
+	public static String dogWalking = "etc/images/char_dog_walking.gif";
+
+}
